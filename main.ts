5bd4e78c467a0fc15c257fcbd940db19f6509503
Tubechon <114917748+Tubechon@users.noreply.github.com>
2022-10-19 14:53:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+let number = 0
+let cout = 0
+input.onButtonPressed(Button.A, function () {
+    number += 1
+    cout += 1
+    basic.showNumber(number)
+})
+input.onButtonPressed(Button.AB, function () {
+    while (number > 0) {
+        number += -1
+        basic.showNumber(number)
+    }
+    while (number == 0) {
+        basic.showNumber(cout)
+    }
+    while (cout > 0) {
+        cout += -1
+        basic.showNumber(cout)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    number += 10
+    basic.showNumber(number)
+})
 basic.forever(function () {
 	
 })
